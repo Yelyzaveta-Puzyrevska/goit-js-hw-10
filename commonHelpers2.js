838fb3a12dabc7d70f1cb09b6897ca7cbfa09e39
Yelyzaveta-Puzyrevska <169177255+Yelyzaveta-Puzyrevska@users.noreply.github.com>
2025-08-24 18:04:42 +0000

    Deploying to gh-pages from @ Yelyzaveta-Puzyrevska/goit-js-hw-10@39eeeb7ab11c1131665aa72a05e37255f5251890 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i as o}from"./assets/vendor-BbbuE1sJ.js";const r={formEl:document.querySelector(".form"),bntEl:document.querySelector("button")};r.formEl.addEventListener("submit",m=>{m.preventDefault();const s=new FormData(r.formEl),e=s.get("delay"),i=s.get("state")==="fulfilled";new Promise((t,n)=>{setTimeout(()=>{i?t():n()},e)}).then(t=>o.success({message:`Fulfilled promise in ${e}ms`})).catch(t=>o.error({message:`Rejected promise in ${e}ms`}))});
+//# sourceMappingURL=commonHelpers2.js.map
